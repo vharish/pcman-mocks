bc235322cfbae58f40bdb9da95494efdd5056026
VenkataHarish <i_v_harish@yahoo.co.in>
2014-05-23 23:19:07 +0530

    Menu changes

--- js/common.js
+++ js/common.js
@@ -1,17 +1,13 @@
 $(document).ready(function(){
     $(".menu").on('tap',toggleMenuView);
     $(".nav>li").on('tap',function(){
-            toggleMenuView();
+            $(".nav-bar-expanded").hide();
             showPage(this);
         });
 });
 
 function toggleMenuView() {
-    if($(".nav-bar-expanded").css("display") === "none"){
-        $(".nav-bar-expanded").css("display","block");
-    } else {
-        $(".nav-bar-expanded").css("display","none");
-    }
+    $(".nav-bar-expanded").toggle();
 }
 
 function showPage(mi) {
