b22a119c33d10c0f5d4f5ef564a390c03420057a
VenkataHarish <i_v_harish@yahoo.co.in>
2014-05-23 23:04:11 +0530

    Moved to single page

--- js/common.js
+++ js/common.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
     $(".menu").on('tap',toggleMenuView);
+    $(".nav>li").on('tap',function(){
+            toggleMenuView();
+            showPage(this);
+        });
 });
 
 function toggleMenuView() {
@@ -7,5 +11,45 @@ function toggleMenuView() {
         $(".nav-bar-expanded").css("display","block");
     } else {
         $(".nav-bar-expanded").css("display","none");
+    }
+}
+
+function showPage(mi) {
+    $.each($(".content>div"),setInactive);
+    switch(mi.id) {
+        case 'mi-quotes': 
+            $("header .heading").text("Quotes");
+            $("#quotes").removeClass("inactive");
+            break;
+        case 'mi-answers': 
+            $("header .heading").text("Answers");
+            $("#answers").removeClass("inactive");
+            break;
+        case 'mi-events': 
+            $("header .heading").text("Events");
+            $("#events").removeClass("inactive");
+            break;
+        case 'mi-images': 
+            $("header .heading").text("Images");
+            $("#images").removeClass("inactive");
+            break;
+        case 'mi-about': 
+            $("header .heading").text("About");
+            $("#about").removeClass("inactive");
+            break;
+        case 'mi-contact': 
+            $("header .heading").text("Contact");
+            $("#contact").removeClass("inactive");
+            break;
+        case 'mi-profile': 
+            $("header .heading").text("My Profile");
+            $("#profile").removeClass("inactive");
+            break;
+    }
+}
+
+function setInactive(index,ele) {
+    if(!$(ele).hasClass("inactive")) {
+        $(ele).addClass("inactive");
     }
 }
