bee1b2d1f2b6b1fac77f673ddcebf6defea4e45f
VenkataHarish <i_v_harish@yahoo.co.in>
2014-05-24 00:56:40 +0530

    Header and footer

--- js/common.js
+++ js/common.js
@@ -1,51 +1,73 @@
 $(document).ready(function(){
-    $(".menu").on('tap',toggleMenuView);
-    $(".nav>li").on('tap',function(){
-            $(".nav-bar-expanded").hide();
-            showPage(this);
-        });
+    $(".menu").on('tap',toggleMenu);
+    setLinks();
 });
 
-function toggleMenuView() {
+function toggleMenu() {
     $(".nav-bar-expanded").toggle();
-}
-
-function showPage(mi) {
-    $.each($(".content>div"),setInactive);
-    switch(mi.id) {
-        case 'mi-quotes': 
-            $("header .heading").text("Quotes");
-            $("#quotes").removeClass("inactive");
-            break;
-        case 'mi-answers': 
-            $("header .heading").text("Answers");
-            $("#answers").removeClass("inactive");
-            break;
-        case 'mi-events': 
-            $("header .heading").text("Events");
-            $("#events").removeClass("inactive");
-            break;
-        case 'mi-images': 
-            $("header .heading").text("Images");
-            $("#images").removeClass("inactive");
-            break;
-        case 'mi-about': 
-            $("header .heading").text("About");
-            $("#about").removeClass("inactive");
-            break;
-        case 'mi-contact': 
-            $("header .heading").text("Contact");
-            $("#contact").removeClass("inactive");
-            break;
-        case 'mi-profile': 
-            $("header .heading").text("My Profile");
-            $("#profile").removeClass("inactive");
-            break;
-    }
 }
 
 function setInactive(index,ele) {
     if(!$(ele).hasClass("inactive")) {
         $(ele).addClass("inactive");
     }
+}
+
+function setLinks() {
+
+    $(".event-details").on("tap",function(){
+        $.each($(".content>div"),setInactive);
+        $("header .heading").text("June 21-23, 2014");
+        $("#event-details").removeClass("inactive");
+    })
+
+    $("#mi-quotes").on("tap",function(){
+        toggleMenu();
+        $.each($(".content>div"),setInactive);
+        $("header .heading").text("Quotes");
+        $("#quotes").removeClass("inactive");
+    })
+
+    $("#mi-answers").on("tap",function(){
+        toggleMenu();
+        $.each($(".content>div"),setInactive);
+        $("header .heading").text("Answers");
+        $("#answers").removeClass("inactive");
+    })
+
+    $("#mi-events").on("tap",function(){
+        toggleMenu();
+        $.each($(".content>div"),setInactive);
+        $("header .heading").text("Events");
+        $("#events").removeClass("inactive");
+    })  
+
+    $("#mi-images").on("tap",function(){
+        toggleMenu();
+        $.each($(".content>div"),setInactive);
+        $("header .heading").text("Images");
+        $("#images").removeClass("inactive");
+    })
+
+    $("#mi-about").on("tap",function(){
+        toggleMenu();
+        $.each($(".content>div"),setInactive);
+        $("header .heading").text("About");
+        $("#about").removeClass("inactive");
+    })
+
+    $("#mi-contact").on("tap",function(){
+        toggleMenu();
+        $.each($(".content>div"),setInactive);
+        $("header .heading").text("Contact");
+        $("#contact").removeClass("inactive");
+    })
+
+    $("#mi-profile").on("tap",function(){
+        toggleMenu();
+        $.each($(".content>div"),setInactive);
+        $("header .heading").text("My Profile");
+        $("#profile").removeClass("inactive");
+    })
+
 }
